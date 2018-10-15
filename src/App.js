a5a2e9e51bcea570f907dd68f988fd7425fdaba3
lazydancer <jrpucula@gmail.com>
2018-10-15 15:13:32 -0400

    Calendar and sidebar added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Sidebar from './Sidebar'
+import Dnd from './DraganddropCalendar'
+
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Sidebar /> 
+        <Dnd />
+        
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <p> Hello </p>
+  </div>
+)
+
+export default Sidebar
